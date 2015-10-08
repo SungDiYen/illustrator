3037bf8c1af23af2f8d20c5fba624a931dfdc797
root <root@ip-172-31-19-37.us-west-2.compute.internal>
2015-10-08 03:32:25 +0000

    por update!!!!

--- app.js
+++ app.js
@@ -1,75 +1,70 @@
-//var AWS = require('aws-sdk');
 var express = require('express');
 var path = require('path');
-var favicon = require('serve-favicon');
+//var favicon = require('static-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+//var http = require('http');
+//var httpProxy = require('http-proxy');
 
-/// custom plugin
-var sassMiddleware = require('node-sass-middleware');
-///
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
 
+
+
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hjs');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+//app.use(favicon());
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+//app.use(require('less-middleware')(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
 
-// catch 404 and forward to error handler
+
+/// catch 404 and forwarding to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
 });
 
-// error handlers
+/// error handlers
 
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
+    app.use(function(err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
     });
-  });
 }
 
 // production error handler
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
 });
 
-///sass_start
-app.use(sassMiddleware({
-  /*Option*/
-    src: __dirname,
-    dest: path.join(__dirname, 'public'),
-    debug: true,
-    outputStyle: 'compressed',
-    prefix:  '/prefix'  // Where prefix is at <link rel="stylesheets" href="prefix/style.css"/>`
-}))
-app.use(express.static(path.join(__dirname, 'public')));
-///sass_end
 
+/*
+    AWS 白名單也要開放IP,port
+*/
+app.listen(8080);
 module.exports = app;
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,9 @@
 var express = require('express');
-var router = express.Router();
+var router  = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/users', function(req, res) {
+  res.render('users', { title: 'userssss'});
 });
 
 module.exports = router;
