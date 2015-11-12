db548551e259e468d84bc71bdecaca87412f3be4
root <root@ip-172-31-19-37.us-west-2.compute.internal>
2015-11-12 17:20:32 +0000

    進度30％ 基本版面設定 grid

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router  = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express_1121' });
+  res.render('index', { title: 'SungDiYen 宋狄諺' });
 });
 
 
